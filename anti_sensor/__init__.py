8a64291b55a025d15785d996d7ce7ea0dc756c4c
Yuxi Wu <yuxiwu@uchicago.edu>
2018-03-12 18:14:53 -0500

    adding init

---
+++ anti_sensor/__init__.py
@@ -0,0 +1,1 @@
+from sensing import sense
--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request
 from app import app
 from form import UrlForm
-from anti_sensor import traffic, localization
+from anti_sensor import sensing
 
 @app.route("/model")
 @app.route('/')
@@ -15,25 +15,8 @@ def homepage():
 @app.route("/results.html", methods=["post"])
 def results():
     try:
-        side1 = get_packets('side1')
-        side2 = get_packets('side2')
-        side2['Time'] = side2['Time'] + 10
-        side3 = get_packets('side3')
-        side3['Time'] = side3['Time'] + 20
-        side4 = get_packets('side4')
-        side4['Time'] = side4['Time'] + 30
-
-        for i in [side1, side2, side3, side4]:
-            preprocess(i)
-
-        df = pd.concat([side1, side2, side3, side4])
-        devices = all_device_stats(df)
-
-        results = []
-        for d in devices:
-            results += (d, fit(d, int(request.form['Length']), int(request.form['Width'])))
-
-        results = pd.DataFrame(dic(results)).to_html()
+        results = sense(int(request.form['Length']), int(request.form['Width']))
+        results = {'results': pd.DataFrame(results).to_html()}
 
     except AssertionError:
         results = {'none': ['']}
